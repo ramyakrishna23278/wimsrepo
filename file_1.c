1f4db88492ca6c253092a31c196cd7c1c925b6d3
Wims2023 <2020wa86453@wilp.bits-pilani.ac.in>
2024-04-30 22:46:12 +0530

    new_code

--- file_1.c
+++ file_1.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+// Function to add two numbers and return their sum
+int add(int a, int b) {
+    return a + b;
+}
+
 int main() {
-    int num1, num2, sum;
+    int num1, num2, result;
 
     // Ask the user for two numbers
     printf("Enter two numbers to add:\n");
@@ -9,13 +14,11 @@ int main() {
     // Read the numbers from user input
     scanf("%d %d", &num1, &num2);
 
-    // Calculate the sum
-    sum = num1 + num2;
+    // Use the add function to get the sum of num1 and num2
+    result = add(num1, num2);
 
     // Display the result
-    printf("The sum of %d and %d is: %d\n", num1, num2, sum);
+    printf("The sum of %d and %d is: %d\n", num1, num2, result);
 
     return 0;
 }
-
-
